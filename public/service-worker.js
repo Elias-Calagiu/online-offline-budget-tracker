431bf390193d77fdd927e324d96fe7b922e145df
Elias Calagiu <eliasc81@gmail.com>
2021-02-28 06:14:06 -0800

    added fetch self event listener to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,6 @@
+const { error } = require("console");
+const { response } = require("express");
+
 const CACHE_NAME = "static-cache-v3";
 const DATA_CACHE_NAME = "data-cache-v2";
 const FILES_TO_CACHE = [
@@ -43,4 +46,39 @@ self.addEventListener('activate', (event) => {
     self.clients.claim()
 
   });
-  
+
+// fetch
+  self.addEventListener('fetch', (event) => {
+    if (event.request.url.includes("/api/")) {
+      event.respondWith(
+        caches
+        ,open(DATA_CACHE_NAME)
+        .then(cache => {
+            return fetch(event.request)
+            .then(response => {
+                if(response.status === 200){
+                    cache.put(event.request.url, response.clone())
+                }
+                return response;
+            }).catch(err => {
+                return cache.match(event.request)
+            })
+        }).catch(err => console.log(err))
+        )
+    } else {
+        event.respondWith(
+            caches
+            .open(CACHE_NAME)
+            .then(cache => {
+                return cache.match(event.request).then(response => {
+                    return response || fetch(event.request)
+                })
+            })
+        )
+    }
+})
+    
+    
+
+    
+          
