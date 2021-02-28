657f69fcbe34b2fbd58352098b4960baf1004e2c
Elias Calagiu <eliasc81@gmail.com>
2021-02-28 05:33:11 -0800

    cached all files present in public folder

---
+++ public/service-worker.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = "static-cache-v3";
+const DATA_CACHE_NAME = "data-cache-v2";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.webmanifest",
+  "/styles.css",
+  "/db.js",
+  "/index.js",
+  "/icons/icon-512x512.png",
+  "/icons/icon-192x192.png"
+];
